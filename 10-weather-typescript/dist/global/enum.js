377ff20aa83518224e5431ec40ec1ce6811e4bd3
Jagmesh <justagoodman312@gmail.com>
2024-01-14 23:11:32 +0300

    chore: remove /dist

--- 10-weather-typescript/dist/global/enum.js
+++
@@ -1,9 +0,0 @@
-export const STORAGE_KEYS = {
-    TOKEN: 'token',
-    CITY: 'city',
-    LANGUAGE: 'language'
-};
-export const LANGUAGE = {
-    RUSSIAN: 'ru',
-    ENGLISH: 'eng'
-};
--- 10-weather-typescript/dist/services/index.js
+++
@@ -1,5 +0,0 @@
-import { LogService } from "./log.service.js";
-import { getKeyValue } from "./storage.service.js";
-import { getWeatherByCity } from "./weather/weather.service.js";
-import { addCity, deleteCities, saveCity, saveLanguage, saveToken } from "./config-cli.service.js";
-export { LogService, getKeyValue, getWeatherByCity, saveCity, deleteCities, saveLanguage, saveToken, addCity };
